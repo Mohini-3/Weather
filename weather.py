5b838224bcf96422199f3c2373db29bb53b528fb
Mohini <mgeda@gitam.in>
2025-07-11 22:10:12 +0530

    Single input from the text file

--- weather.py
+++ weather.py
@@ -1,10 +1,15 @@
 def temperature(m,n,p,t):
     temp=m*t**2+n*t+p
     return temp
-#keyboard input
-m=float(input("enter m:"))
-n=float(input("enter n:"))
-p=float(input("enter p:"))
-t=int(input("enter time:"))
-temp=temperature(m,n,p,t)
-print("The temperature is",temp)
+#Single input from the text file
+with open('file.txt', 'r') as files:
+    file = files.readline()
+    m, n, p, t = file.split()
+    m = float(m)
+    n = float(n)
+    p = float(p)
+    t = int(t)
+
+    temp = temperature(m, n, p, t)
+    print(f'The Temperature at time {t} is {temp}')
+
